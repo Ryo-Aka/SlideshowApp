d52d1633714ec1e9476249299a1885b5160c31ef
Ryo KnoableJP <knoablejp-ryo@KnoableJP-RyonoMacBook-Pro.local>
2017-08-30 12:54:47 +0900

    再提出修正　//内は修正前

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let image0:UIImage = UIImage(named: "春.jpg")!
-    let image1:UIImage = UIImage(named: "夏.jpg")!
-    let image2:UIImage = UIImage(named: "秋.jpg")!
-    let image3:UIImage = UIImage(named: "冬.jpg")! //画像を準備
+    let images:[UIImage] = [
+        UIImage(named: "春.jpg")!,
+        UIImage(named: "夏.jpg")!,
+        UIImage(named: "秋.jpg")!,
+        UIImage(named: "冬.jpg")!,
+        ]
+    
+    //let image0:UIImage = UIImage(named: "春.jpg")!
+    //let image1:UIImage = UIImage(named: "夏.jpg")!
+    //let image2:UIImage = UIImage(named: "秋.jpg")!
+    //let image3:UIImage = UIImage(named: "冬.jpg")! //画像を準備
     
     var timer: Timer! //タイマー用
     
     var num = 0 //画像のスライド用
     
 
-    @IBOutlet weak var botton_name: UIButton! //再生、停止ボタン用
+    @IBOutlet weak var botton_name: UIButton! //再生、停止ボタン
     
-       @IBOutlet weak var screen_image: UIButton! //画像ボタン用
+    @IBOutlet weak var screen_image: UIButton! //画像ボタン
     
+    @IBOutlet weak var next_botton: UIButton! //進むボタン
     
+    @IBOutlet weak var back_botton: UIButton! //戻るボタン
     
     //上記は定義　以下機能
 
@@ -32,7 +41,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-        screen_image.setImage(image0, for: .normal)
+        screen_image.setImage(images[0], for: .normal)
         
         
         botton_name.setTitle("再生", for: .normal)
@@ -50,20 +59,22 @@ class ViewController: UIViewController {
         if self.timer == nil{
         num += 1
         
-        if(num%4 == 0){
-            screen_image.setImage(image0, for: .normal)
-        }
-        else if(num%4 == 1){
-            screen_image.setImage(image1, for: .normal)
-        }
-        else if(num%4 == 2){
-            screen_image.setImage(image2, for: .normal)
-        }
-        else if(num%4 == 3){
-            screen_image.setImage(image3, for: .normal)
-        }
-        else {
-        }
+            screen_image.setImage(images[num % images.count], for: .normal)
+            
+        //if(num%4 == 0){
+          //  screen_image.setImage(image0, for: .normal)
+        //}
+        //else if(num%4 == 1){
+          //  screen_image.setImage(image1, for: .normal)
+        //}
+        //else if(num%4 == 2){
+           // screen_image.setImage(image2, for: .normal)
+        //}
+        //else if(num%4 == 3){
+          //  screen_image.setImage(image3, for: .normal)
+        //}
+        //else {
+        //}
         }
     } //進むボタン
     
@@ -71,53 +82,73 @@ class ViewController: UIViewController {
         if self.timer == nil{
         num += 3
         
-        if(num%4 == 0){
-            screen_image.setImage(image0, for: .normal)
-        }
-        else if(num%4 == 1){
-            screen_image.setImage(image1, for: .normal)
-        }
-        else if(num%4 == 2){
-            screen_image.setImage(image2, for: .normal)
-        }
-        else if(num%4 == 3){
-            screen_image.setImage(image3, for: .normal)
-        }
-        else {
-        }
+          screen_image.setImage(images[num % images.count], for: .normal)
+            
+       // if(num%4 == 0){
+         //   screen_image.setImage(image0, for: .normal)
+        //}
+        //else if(num%4 == 1){
+          //  screen_image.setImage(image1, for: .normal)
+        //}
+        //else if(num%4 == 2){
+          //  screen_image.setImage(image2, for: .normal)
+        //}
+        //else if(num%4 == 3){
+          //  screen_image.setImage(image3, for: .normal)
+        //}
+        //else {
+        //}
         }
     } //戻るボタン
     
     func slide(timer: Timer!) {
         num += 1
         
-        if(num%4 == 0){
-            screen_image.setImage(image0, for: .normal)
-        }
-        else if(num%4 == 1){
-            screen_image.setImage(image1, for: .normal)
-        }
-        else if(num%4 == 2){
-            screen_image.setImage(image2, for: .normal)
-        }
-        else if(num%4 == 3){
-            screen_image.setImage(image3, for: .normal)
-        }
-        else {
-        }
+        screen_image.setImage(images[num % images.count], for: .normal)
+        
+        //if(num%4 == 0){
+          //  screen_image.setImage(image0, for: .normal)
+        //}
+        //else if(num%4 == 1){
+          //  screen_image.setImage(image1, for: .normal)
+        //}
+        //else if(num%4 == 2){
+          //  screen_image.setImage(image2, for: .normal)
+        //}
+        //else if(num%4 == 3){
+          //  screen_image.setImage(image3, for: .normal)
+        //}
+        //else {
+        //}
     } //スライドショー　タイマー用関数
     
     @IBAction func start_stop(_ sender: Any) {
         if self.timer == nil {
+            next_botton.isEnabled = false
+            back_botton.isEnabled = false
             botton_name.setTitle("停止", for: .normal)
             self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(slide), userInfo: nil, repeats: true)
         }
         else if self.timer != nil {
+            next_botton.isEnabled = true
+            back_botton.isEnabled = true
             botton_name.setTitle("再生", for: .normal)
             self.timer.invalidate()
             self.timer = nil
     }
     } //再生、停止ボタン　テキストの切り替わり タイマー設定
+    
+    @IBAction func screen_botton(_ sender: Any) {
+        if self.timer != nil {
+            next_botton.isEnabled = true
+            back_botton.isEnabled = true
+            botton_name.setTitle("再生", for: .normal)
+            self.timer.invalidate()
+            self.timer = nil
+        }
+        else{
+        }
+    } //画像を押した時にスライドショーが再生している場合、ストップしておく
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             let exViewController = segue.destination as! exViewController
